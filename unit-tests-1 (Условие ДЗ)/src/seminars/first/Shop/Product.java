b1f74e69ffe40377e8c693d05b2db4a1e9d0ec41
Valetov Andrei <graf.any@gmail.com>
2023-12-24 10:39:09 +0300

    HW 1

--- unit-tests-1 (Условие ДЗ)/src/seminars/first/Shop/Product.java
+++ unit-tests-1 (Условие ДЗ)/src/seminars/first/Shop/Product.java
@@ -4,6 +4,10 @@ public class Product {
     private Integer cost; // Стоимость продукта
     private String title; // Название
 
+    public Product(Integer cost, String title) {
+        this.cost = cost;
+        this.title = title;
+    }
     // Геттеры, сеттеры:
     public int getCost() {
         return cost;
@@ -19,5 +23,27 @@ public class Product {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+    @Override
+    public String toString() {
+        return String.format("%s - %d", title, cost);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+
+        Product product = (Product) o;
+
+        if (getCost() != (product.getCost())) return false;
+        return getTitle().equals(product.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getCost();
+        result = 31 * result + getTitle().hashCode();
+        return result;
     }
 }
--- unit-tests-1 (Условие ДЗ)/src/seminars/first/Shop/Shop.java
+++ unit-tests-1 (Условие ДЗ)/src/seminars/first/Shop/Shop.java
@@ -1,7 +1,10 @@
 package seminars.first.Shop;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Shop {
     private List<Product> products;
@@ -18,13 +21,27 @@ public class Shop {
     // Метод должен вернуть отсортированный по возрастанию по цене список продуктов
     public List<Product> sortProductsByPrice() {
         // Допишите реализацию метода самостоятельно
-        return null;
+        if (products == null || products.size() == 0) return null;
+
+        List<Product> sortedList = new ArrayList<>(products);
+        sortedList.sort(Comparator.comparingInt(Product::getCost));
+        return sortedList;
     }
 
     // Метод должен вернуть самый дорогой продукт
     public Product getMostExpensiveProduct() {
         // Допишите реализацию метода самостоятельно
-        return null;
-    }
 
+        if (products == null || products.size() == 0) return null;
+
+        Product mostExpensiveProduct = new Product(0, "");
+        for (Product product : products) {
+            if (product.getCost() > mostExpensiveProduct.getCost()) {
+                mostExpensiveProduct = product;
+            }
+
+        }
+
+        return mostExpensiveProduct;
+    }
 }
