b22db89dc2c2cb9b6a701ba776353bea16c6c700
Valetov Andrei <graf.any@gmail.com>
2024-01-22 00:32:45 +0300

    TASK 6 test_list+

--- pythonProject-main-Код-с-семинара/main.py
+++ pythonProject-main-Код-с-семинара/main.py
@@ -1,5 +1,25 @@
 # This is a sample Python script.
 from Tasks import Tasks
+import random
+
+from list_average import ListAverage
+
+
+def main():
+    la = ListAverage.ListAverage(random.sample(range(100), 10), random.sample(range(100), 10))
+    average0 = la.average_first_list
+    average1 = la.average_second_list
+
+    print(f"Первый список: {la.first_list}")
+    print(f"Второй список: {la.second_list}")
+    print()
+
+    if average0 > average1:
+        print(f"Первый список имеет большее среднее значение ({average0}) чем второй список ({average1})")
+    elif average1 > average0:
+        print(f"Второй список имеет большее среднее значение ({average1}) чем первый список ({average0})")
+    else:
+        print(f"Средние значения первого и второго списка равны {average0}")
 
 
 def print_hi(name):
@@ -8,6 +28,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    main()
     # Примеры использования функций:
     # 1
     avg = Tasks.find_average([1, 2, 3, 4, 5])
